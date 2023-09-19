e6ac2d5da4e5157ed496af4894bcb46bda311907
webtech <purposeresearch923@gmail.com>
2023-09-19 18:01:09 +0530

    hello

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import AboutUS from './AboutUS'
+import CounterRecord from './CounterRecord'
 
 function About() {
   return (
      <> 
      <Header/> 
      <AboutUS/> 
+     <CounterRecord/>
      <Footer/>
      </>
   )
--- src/Pages/About/AboutUS.js
+++ src/Pages/About/AboutUS.js
@@ -1,18 +1,44 @@
 import React from 'react'
 import aboutimg from '../../Images/home-about-img.webp'
+import clientlogos from '../../Images/nucosmetic.webp'
 
 function AboutUS() {
     return (
         <>
             <div className="container-fluid aboutus-main">
                 <div className="container-xl">
-                    <div className="row align-items-center">
+                    <div className="row align-items-center justify-content-center">
                         <div className="col-lg-7 aboutus-content">
-                                <h1>About <span>Us</span> </h1>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto officia dolor nostrum consequatur, obcaecati officiis tempora voluptatem vitae illum, totam exercitationem voluptas. Quae, cupiditate voluptates reprehenderit officiis eius fugiat ratione omnis illum harum impedit minima eos non ducimus autem delectus itaque quam corporis quo porro repellendus fugit rerum ipsa. Sint aperiam esse excepturi laborum labore et praesentium quisquam laboriosam eveniet, recusandae eaque cupiditate quam alias maxime consectetur omnis sunt quos hic. Autem necessitatibus quos odio corrupti sequi magni earum, reiciendis labore eligendi eum voluptas accusantium nobis voluptatum natus voluptatibus! Numquam omnis expedita aut quas rem repellendus magnam veritatis voluptatibus nemo exercitationem velit totam quasi earum id, beatae officia veniam. Explicabo dignissimos provident inventore vero iste nobis ea natus odit nulla incidunt. </p>
+                            <h1>About <span>Us</span> </h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto officia dolor nostrum consequatur, obcaecati officiis tempora voluptatem vitae illum, totam exercitationem voluptas. Quae, cupiditate voluptates reprehenderit officiis eius fugiat ratione omnis illum harum impedit minima eos non ducimus autem delectus itaque quam corporis quo porro repellendus fugit rerum ipsa. Sint aperiam esse excepturi laborum labore et praesentium quisquam laboriosam eveniet, recusandae eaque cupiditate quam alias maxime consectetur omnis sunt quos hic. Autem necessitatibus quos odio corrupti sequi magni earum, reiciendis labore eligendi eum voluptas accusantium nobis voluptatum natus voluptatibus! Numquam omnis expedita aut quas rem repellendus magnam veritatis voluptatibus nemo exercitationem velit totam quasi earum id, beatae officia veniam. Explicabo dignissimos provident inventore vero iste nobis ea natus odit nulla incidunt.</p>
                         </div>
-                        <div className="col-lg-5">
+                        <div className="col-lg-5 col-md-6 col-sm-8">
                             <img src={aboutimg} alt="" className='img-fluid' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container-fluid bg-dark">
+                <div className="container-xl">
+                    <div className="row py-4 text-center my-5">
+                        <div className="col-lg-2 col-md-2 col-sm-3 col-4 mb-3 mb-lg-0">
+                            <img src={clientlogos} alt="" width={90}/>
+                        </div>
+                        <div className="col-lg-2 col-md-2 col-sm-3 col-4 mb-3 mb-lg-0">
+                            <img src={clientlogos} alt="" width={90}/>
+                        </div>
+                        <div className="col-lg-2 col-md-2 col-sm-3 col-4 mb-3 mb-lg-0">
+                            <img src={clientlogos} alt="" width={90}/>
+                        </div>
+                        <div className="col-lg-2 col-md-2 col-sm-3 col-4 mb-3 mb-lg-0">
+                            <img src={clientlogos} alt="" width={90}/>
+                        </div>
+                        <div className="col-lg-2 col-md-2 col-sm-3 col-4 mb-3 mb-lg-0">
+                            <img src={clientlogos} alt="" width={90}/>
+                        </div>
+                        <div className="col-lg-2 col-md-2 col-sm-3 col-4 mb-3 mb-lg-0">
+                            <img src={clientlogos} alt="" width={90}/>
                         </div>
                     </div>
                 </div>
