e490f5feb2f8ae20d829582bbe5b78fc18b76c7a
webtech <purposeresearch923@gmail.com>
2023-09-20 15:36:00 +0530

    hello

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,13 +3,17 @@ import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import AboutUS from './AboutUS'
 import CounterRecord from './CounterRecord'
+import WhyChooseUs from './WhyChooseUs'
+import Clients from'../Home/Clients'
 
 function About() {
   return (
      <> 
      <Header/> 
      <AboutUS/> 
-     <CounterRecord/>
+     <WhyChooseUs/>
+     <CounterRecord/> 
+     <Clients/>
      <Footer/>
      </>
   )
---
+++ src/Pages/About/WhyChooseUs.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import aboutimg from '../../Images/home-about-img.webp'
+
+
+function WhyChooseUs() {
+    return (
+        <>
+
+            <div className="container-fluid ">
+                <div className="container-xl">
+                    <div className="row align-items-center justify-content-lg-between justify-content-center">
+                        <div className="col-lg-5 col-md-6 col-sm-6 col-8">
+                            <img src={aboutimg} alt="" className='img-fluid' />
+                        </div>
+                        <div className="col-lg-6 col-md-12 col-sm-12 why-choose py-3">
+                            <div>
+                                <h1>Why <span>Choose Us?</span> </h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto officia dolor nostrum consequatur, obcaecati officiis tempora voluptatem vitae illum, totam exercitationem voluptas obcaecati officiis tempora voluptatem vitae illum, totam exercitationem voluptas exercitationem voluptas obcaecati officiis tempora voluptatem vitae illum, totam exercitationem voluptas.  </p>
+                            </div>
+                            <div className="why-choose-checkbox">
+                                <p><i class="fa-solid fa-check"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <p><i class="fa-solid fa-check"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <p><i class="fa-solid fa-check"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default WhyChooseUs
