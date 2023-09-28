5790ec8dbb04b948af058d3029cea447829db0bc
webtech <purposeresearch923@gmail.com>
2023-09-28 15:29:46 +0530

    hello

--- src/Pages/Services/AppDevelopment/AppServiceOffer.js
+++ src/Pages/Services/AppDevelopment/AppServiceOffer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import android from './../../../Images/android.png'
+import ios from './../../../Images/IOS.png'
 
 function ServiceOffer() {
     return (
@@ -6,23 +8,47 @@ function ServiceOffer() {
             <div className="container-fluid service-box-bg">
                 <div className="container-xl">
                     <div className="row">
-                            <div className="col-lg-6 col-md-8">
-                                <div className='serviceOffer'>
-                                    <h1>Service <span>We Offer</span></h1>
-                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis illo, error, beatae, quod ut minus molestias ea sint quae id quaerat quis et eligendi blanditiis placeat. Fuga esse ipsa ad. beatae, quod ut minus molestias ea sint quae id quaerat quis et eligendi blanditiis placeat. Fuga esse ipsa ad beatae, quod ut minus molestias ea sint quae id quaerat quis et eligendi blanditiis placeat. </p>
-                                </div>
+                        <div className="col-lg-6 col-md-8">
+                            <div className='serviceOffer'>
+                                <h1>Service <span>We Offer</span></h1>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis illo, error, beatae, quod ut minus molestias ea sint quae id quaerat quis et eligendi blanditiis placeat. Fuga esse ipsa ad. beatae, quod ut minus molestias ea sint quae id quaerat quis et eligendi blanditiis placeat. Fuga esse ipsa ad beatae, quod ut minus molestias ea sint quae id quaerat quis et eligendi blanditiis placeat. </p> <br />
+                                <ul>
+                                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li> 
+                                </ul>
+                                 
                             </div>
+                        </div>
                         <div className="col-lg-3 col-md-4 col-sm-6">
                             <div className='serviceOffer-box'>
+                                <img src={android} alt="" />
                                 <h2>Android App Development</h2>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> <br /> 
+                                <ul>
+                                    <li>Lorem ipsum dolor sit.</li> 
+                                    <li>Lorem ipsum dolor sit.</li> 
+                                    <li>Lorem ipsum dolor sit.</li> 
+                                    <li>Lorem ipsum dolor sit.</li> 
+                                </ul>
+                                
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-4 col-sm-6">
                             <div className='serviceOffer-box'>
+                                <img src={ios} alt="" />
                                 <h2>IOS App Development</h2>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> <br /> 
+                                <ul>
+                                    <li>Lorem ipsum dolor sit.</li> 
+                                    <li>Lorem ipsum dolor sit.</li> 
+                                    <li>Lorem ipsum dolor sit.</li> 
+                                    <li>Lorem ipsum dolor sit.</li> 
+                                </ul>
                             </div>
                         </div>
-                         
+
                     </div>
                 </div>
             </div>
--- src/Pages/Services/DigitalMarketing/DigitalMarketing.js
+++ src/Pages/Services/DigitalMarketing/DigitalMarketing.js
@@ -1,30 +1,47 @@
 import React from 'react'
 import Header from '../../Header/Header'
-import Footer from '../../Footer/Footer' 
+import Footer from '../../Footer/Footer'
+import webdevelopmentImg from '../../../Images/web-develop-imng.webp'
+import icon from './../../../Images/Travel.png'
+
 
 function DigitalMarketing() {
   return (
     <>
       <Header />
-      <div className="container-fluid py-5">
+
+
+      <div className="container-fluid web-development-main">
+        <div className="container-xl">
+          <div className="row align-items-center justify-content-center">
+            <div className="col-lg-6 col-md-6 col-sm-8 col-12 mb-3">
+              <img src={webdevelopmentImg} alt="" className='img-fluid' />
+            </div>
+            <div className="col-lg-6 web-development-inner mb-3">
+              <h1><span>Digital Marketing</span> Services</h1>
+              <p>Unleash the power of your online presence with our cutting-edge web development solutions. From stunning designs to seamless functionality, we create websites that captivate your audience and drive success. Elevate your digital game with our expert team. with a great user Interface with the best design for your own website. We help you establish your business, brand and other things online. Elevate your digital game with our expert team. with a great user Interface with the best design for your own website. We help you establish your business, brand and other things online. Elevate your digital game with our expert team. with a great user Interface with the best design for your own website. We help you establish your business, brand and other things online.</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className="container-fluid">
         <div className="container-xl">
           <div className="row">
-            <div className="col-lg-4">
+            <div className="col-lg-3">
               <div className="service-dm-bg">
+                <img src={icon} alt="" width={50}/>
+                <h1>SEO</h1>
               </div>
             </div>
-            <div className="col-lg-4">
-              <div className="service-dm-bg">
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="service-dm-bg">
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="service-dm-bg">
-              </div>
-            </div>
+             
+               
+          
+           
+            
 
           </div>
         </div>
