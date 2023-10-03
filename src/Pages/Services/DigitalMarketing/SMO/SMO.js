3e0854e8d52fe2ef55f7245527897c277173c2f3
webtech <purposeresearch923@gmail.com>
2023-10-03 17:59:11 +0530

    changes

--- src/Pages/Services/DigitalMarketing/SMO/SMO.js
+++ src/Pages/Services/DigitalMarketing/SMO/SMO.js
@@ -10,20 +10,18 @@ function SMO() {
     return (
         <>
             <Header />
-            <div className="container-fluid web-development-main">
-                <div className="container-xl">
-                    <div className="row align-items-center justify-content-center">
-                        <div className="col-lg-6 col-md-6 col-sm-7 col-12 mb-3">
+           
+                    <div className="smo-main">
+                        <div className="smoimg-left">
                             <img src={SMOIMG} alt="" loading='lazy' className='img-fluid' />
                         </div>
-                        <div className="col-lg-6 web-development-inner mb-3">
+                        <div className="  web-development-inner smotext-right">
                             <h1><span>Social Media</span> Optimization</h1>
                             <p>Unleash the power of your online presence with our cutting-edge web development solutions. From stunning designs to seamless functionality, we create websites that captivate your audience and drive success. Elevate your digital game with our expert team. with a great user Interface with the best design for your own website. We help you establish your business, brand and other things online. Elevate your digital game with our expert team. with a great user Interface with the best design for your own website. We help you establish your business, brand and other things online. Elevate your digital game with our expert team. with a great user Interface with the best design for your own website. We help you establish your business, brand and other things online.</p>
 
                         </div>
 
-                    </div>
-                </div>
+                    
             </div>
             <SMOServiceOffer/>
             <SMOGrowBusiness/>
