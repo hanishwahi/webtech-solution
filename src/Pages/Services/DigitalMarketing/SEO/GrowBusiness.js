4d9b7cc0dc21966afb84f17ea669d68ce3dbf7a1
webtech <purposeresearch923@gmail.com>
2023-10-03 13:28:20 +0530

    changes

--- src/Pages/Services/DigitalMarketing/SEO/GrowBusiness.js
+++ src/Pages/Services/DigitalMarketing/SEO/GrowBusiness.js
@@ -44,6 +44,98 @@ function GrowBusiness() {
                 </div>
             </div>
 
+            <div className="container-fluid">
+                <div className="container-xl">
+                    <div className="row seo-work">
+                        <h1>Our Seo Work Includes</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus sunt fuga minima, expedita esse amet quam praesentium voluptatem quas inventore. Quibusdam tenetur corrupti ullam harum, pariatur quam obcaecati facilis maxime?</p>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-4 col-md-6 col-sm-6 mb-4">
+                            <div className="row pb-3">
+                                <div className="col-md-3 col-sm-4 col-3">
+                                    <img src={icon} alt="" width={60} />
+                                </div>
+                                <div className="col-md-9 col-sm-8 col-9 seo-work-text">
+                                    <h3>Website Audit</h3>
+                                </div>
+                            </div>
+                            <div className="row pb-3 seo-work-text">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisi elit. Sint voluptatem eaque nulla saepe, ullam reiciendis ullam reiciendis ullam reiciendis. </p>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-6 mb-4">
+                            <div className="row pb-3">
+                                <div className="col-md-3 col-sm-4 col-3">
+                                    <img src={icon} alt="" width={60} />
+                                </div>
+                                <div className="col-md-9 col-sm-8 col-9 seo-work-text">
+                                    <h3>Keyword Search & Analysis</h3>
+                                </div>
+                            </div>
+                            <div className="row pb-3 seo-work-text">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisi elit. Sint voluptatem eaque nulla saepe, ullam reiciendis ullam reiciendis ullam reiciendis. </p>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-6 mb-4">
+                            <div className="row pb-3">
+                                <div className="col-md-3 col-sm-4 col-3">
+                                    <img src={icon} alt="" width={60} />
+                                </div>
+                                <div className="col-md-9 col-sm-8 col-9 seo-work-text">
+                                    <h3>Link Building</h3>
+                                </div>
+                            </div>
+                            <div className="row pb-3 seo-work-text">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisi elit. Sint voluptatem eaque nulla saepe, ullam reiciendis ullam reiciendis ullam reiciendis. </p>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-6 mb-4">
+                            <div className="row pb-3">
+                                <div className="col-md-3 col-sm-4 col-3">
+                                    <img src={icon} alt="" width={60} />
+                                </div>
+                                <div className="col-md-9 col-sm-8 col-9 seo-work-text">
+                                    <h3>Keyword & URL Optimization</h3>
+                                </div>
+                            </div>
+                            <div className="row pb-3 seo-work-text">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisi elit. Sint voluptatem eaque nulla saepe, ullam reiciendis ullam reiciendis ullam reiciendis. </p>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-6 mb-4">
+                            <div className="row pb-3">
+                                <div className="col-md-3 col-sm-4 col-3">
+                                    <img src={icon} alt="" width={60} />
+                                </div>
+                                <div className="col-md-9 col-sm-8 col-9 seo-work-text">
+                                    <h3>SEO Content Creation</h3>
+                                </div>
+                            </div>
+                            <div className="row pb-3 seo-work-text">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisi elit. Sint voluptatem eaque nulla saepe, ullam reiciendis ullam reiciendis ullam reiciendis. </p>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-6 mb-4">
+                            <div className="row pb-3">
+                                <div className="col-md-3 col-sm-4 col-3">
+                                    <img src={icon} alt="" width={60} />
+                                </div>
+                                <div className="col-md-9 col-sm-8 col-9 seo-work-text">
+                                    <h3>Reporting & Analysis</h3>
+                                </div>
+                            </div>
+                            <div className="row pb-3 seo-work-text">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisi elit. Sint voluptatem eaque nulla saepe, ullam reiciendis ullam reiciendis ullam reiciendis. </p>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
