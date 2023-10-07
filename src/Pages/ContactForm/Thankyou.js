b49ab60adc72360548ee432a6be6bfce58e9be2f
webtech <purposeresearch923@gmail.com>
2023-10-07 12:24:36 +0530

    changes

--- src/Pages/ContactForm/Thankyou.js
+++ src/Pages/ContactForm/Thankyou.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import pdf from '../../Images/WebtechSolutions.pdf'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 
 
 function Thankyou() {
 
     return (
         <>
+            <Header />
 
-
-            <div className="vh-100 d-flex justify-content-center" style={{ backgroundColor: "#e2f5ff" }}>
-                <div className=" border-0 p-5">
+            <div className="d-flex justify-content-center" style={{ backgroundColor: "#e2f5ff" }}>
+                <div className=" border-0 py-5">
                     <div className="mb-4 text-center">
                         <svg xmlns="http://www.w3.org/2000/svg" className="text-success bi bi-check-circle" width="75" height="75"
                             fill="currentColor" viewBox="0 0 16 16">
@@ -19,17 +21,19 @@ function Thankyou() {
                                 d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
                         </svg>
                     </div>
-                    <div className="text-center">
-                        <h1>Thank You for Contacting Us !</h1>
-                        <p>We will get in touch with you soon.</p>
-                        <Link className='backTohome' to='/'>Back to home</Link> <br /><br />
-                        <a type="application/pdf" href={pdf} download>Download the pdf</a><br /><br />
- 
+                    <div className="text-center thankyou-para py-3">
+                        {/* <h1>Thank You for Contacting Us !</h1>
+                        <p>We will get in touch with you soon.</p> */}
+                        <p>Thank you for sharing your requirements with us. While we review your information, we invite you to download our company profile in PDF format. Simply click the link below to get a better understanding of our Company, Work and capabilities</p><br />
 
-             
+                        <a type="application/pdf" href={pdf} download>Download the pdf</a><br /><br />
                     </div>
+                    {/* <div className='text-center'>
+                        <Link className='backTohome' to='/'>Back to home</Link>
+                    </div> */}
                 </div>
             </div>
+            <Footer/>
         </>
     )
 }
