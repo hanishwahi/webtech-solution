cfed46a0d5116fd5f01937ab691dbc5fab138038
webtech <purposeresearch923@gmail.com>
2023-10-17 17:54:25 +0530

    first commit

--- src/Pages/ContactForm/InputForm.js
+++ src/Pages/ContactForm/InputForm.js
@@ -87,7 +87,7 @@ function InputForm() {
                     <label className="form-label fw-bold" htmlFor="form4Example3">*Message</label>
                     <textarea className="form-control" name="message" required id="form4Example6" rows="2" placeholder='Message...'></textarea>
                 </div>
-                <input onClick={scrollToHome}  className='form-Button' type="submit" value="Submit" />
+                <input className='form-Button' type="submit" value="Submit" />
             </form>
             {/* <ToastContainer /> */}
         </>
