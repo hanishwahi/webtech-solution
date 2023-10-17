0ed1ff44fe0f96db788839b650192968e63f5bc8
webtech <purposeresearch923@gmail.com>
2023-10-17 17:16:37 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('AW-11163670422');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
