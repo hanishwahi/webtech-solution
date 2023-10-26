fc772ec395d9b9add168f09a7f6f48aa29fd5f5f
webtech <purposeresearch923@gmail.com>
2023-10-26 17:42:50 +0530

    first commit

--- src/Pages/Home/Ceritified.js
+++ src/Pages/Home/Ceritified.js
@@ -6,34 +6,30 @@ import google from '../../Images/googleCeritified.webp'
 function Ceritified() {
     return (
         <>
-            <div className="container-fluid py-5">
+            <div className="container-fluid core-values-bg">
                 <div className="container">
                     <div className="row">
                         <div className="col certified">
                             <h1>We are <span>Ceritified with</span></h1>
                         </div>
                     </div>
-                    <div className="row align-items-center justify-content-around">
-                        <div className="col-lg-3">
-                            <div className='shadow border'>
-                                <img src={amazon} alt="" className='img-fluid' />
+                    <div className="row align-items-center justify-content-around text-center">
+                        <div className="col-md-4 col-sm-4 col-6 mb-3">
+                            <div className=''>
+                                <img src={amazon} alt="" width={160} />
                             </div>
                         </div>
-                        <div className="col-lg-3">
-                            <div className='shadow border'>
-                                <img src={flipkart} alt="" className='img-fluid' />
+                        <div className="col-md-4 col-sm-4 col-6 mb-3">
+                            <div className=''>
+                                <img src={flipkart} alt="" width={160} />
                             </div>
                         </div>
-                        <div className="col-lg-3">
-                            <div className='shadow border'>
-                                <img src={google} alt="" className='img-fluid' />
+                        <div className="col-md-4 col-sm-4 col-6 mb-3">
+                            <div className=''>
+                                <img src={google} alt="" width={160} />
                             </div>
                         </div>
-                        <div className="col-lg-3">
-                            <div className='shadow border'>
-                                <img src={google} alt="" className='img-fluid' />
-                            </div>
-                        </div>
+                         
                     </div>
                 </div>
             </div>
