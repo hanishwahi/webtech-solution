abc35c46742890532527208e2e642ce89e93577e
webtech <purposeresearch923@gmail.com>
2023-11-06 17:52:50 +0530

    first commit

--- src/Pages/Career/Career.js
+++ src/Pages/Career/Career.js
@@ -19,32 +19,28 @@ function Career() {
                                 <div className="accordion-item">
                                     <h2 className="accordion-header" id="headingOne">
                                         <button className="accordion-button shadow-none fw-bold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                                            SEO Executive - Immediate Joining
+                                            Pre Sales Executive
                                         </button>
                                     </h2>
                                     <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                                         <div className="accordion-body pb-0">
-                                            <div className='mb-3'>
-                                                <h1 className='h5'>Job description:</h1>
-                                                <p>We are searching for a passionate SEO Executive to join our team. The SEO Executive responsibilities include analyzing and identifying gaps in website content, optimizing pages for improved search results, building links, and supervising content creation. You should be able to identify marketing opportunities and come up with strategies to increase website traffic.</p>
-                                            </div>
                                             <div className='mb-4'>
                                                 <h1 className='h5'>Roles And Responsibility:</h1>
-                                                <p>Conducting keyword research using dedicated software, and generating new keyword ideas. Analyzing website and social media pages, then making recommendations for improvement. Monitoring website traffic, search results, and developing strategies. Generating content ideas and delegating these to the team. Overseeing content creation and offering suggestions. Updating outdated content. Optimizing pages for desktop, tablet, and mobile use. Developing link-building strategies. Managing paid search campaigns. Keeping up to date with new trends and best SEO practices.</p>
+                                                <p>Contact potential clients through phone calls, emails, or social media.</p>
+                                                <p>Identify their needs and requirements and match them with our services.</p>
+                                                <p>Provide them with relevant information and persuade them to schedule a discovery call with our experts.</p>
+                                                <p>Maintain a database of leads and track their progress.</p>
+                                                <p>Collaborate with the sales and marketing teams to achieve sales targets.</p>
+                                                <p>Ability to work independently and as part of a team .</p>
                                             </div>
                                             <div className='mb-4 jobrequiremnt'>
-                                                <h1 className='h5'>SEO Executive Requirements:</h1>
-                                                <ul>
-                                                    <li>Previous experience in a similar role would be advantageous.</li>
-                                                    <li>Excellent written, verbal, and analytical skills.</li>
-                                                    <li>Great customer service and interpersonal skills.</li>
-                                                    <li>Ability to follow specifications.</li>
-                                                    <li>Ability to work under pressure.</li>
-                                                </ul>
+                                                <h1 className='h5'>Skills:</h1>
+                                                <p> English Communication skills, Knowledge of Designing software, and understand client's requirements clearly.</p>
+                                                <p>Some knowledge about web development and digital marketing .</p>
                                             </div>
                                             <div>
-                                                <p className='mb-1'><strong>Experience :</strong>  Fresher </p>
-                                                <p className='mb-1'><strong>No of Position :</strong>  5 </p>
+                                                <p className='mb-1'><strong>Experience :</strong> Freshers can also apply</p>
+                                                <p className='mb-1'><strong>No of Position :</strong>  1 </p>
                                                 <p className='mb-1'> <strong>Location :</strong>  Mohali</p>
                                                 <p className='mb-1'><strong>Interested Candidate call at :</strong><Link to="tel:+91 8427682603"> +91 8427682603</Link></p>
 
@@ -57,9 +53,10 @@ function Career() {
                                     </div>
                                 </div>
                             </div>
-                          
+
                         </div>
                     </div>
+                   
                     <div className='row'>
                         <div className='col-lg-12 mb-4'>
                             <div className="accordion" id="accordionExample">
@@ -71,20 +68,20 @@ function Career() {
                                     </h2>
                                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                                         <div className="accordion-body pb-0">
-                                        <div className='mb-3'>
+                                            <div className='mb-3'>
                                                 <h1 className='h5'>Job Description:</h1>
                                                 <p> We are seeking a dynamic and results-driven Business Development Manager to join our team. The ideal candidate will be responsible for identifying new business opportunities, cultivating relationships with potential clients, and expanding our market presence. This role requires a strategic thinker with strong communication and negotiation skills, as well as the ability to drive revenue growth.
-</p>
+                                                </p>
                                             </div>
                                             <div className='mb-3'>
                                                 <h1 className='h5'>Roles & Responsibilities:</h1>
                                                 <p>Identify and research potential business opportunities conduct market research, analyse industry trends, and identify potential clients or target markets for the organization's products or services. <br /> <br />
-                                                Lead generation and prospecting proactively reach out to potential clients through various channels such as cold calling, emails, networking events, and social media to generate leads and establish initial contact. <br /><br />
-                                                Relationship building Build and maintain strong relationships with clients, key decision-makers, and stakeholders. Understand their needs, requirements, and pain points to provide suitable solutions and position the organization's offerings effectively. <br /><br />
-                                                Client management act as a point of contact for clients, ensuring high levels of customer satisfaction. Address client concerns, resolve issues, and provide ongoing support to build long-term relationships and foster repeat business.
-</p>
+                                                    Lead generation and prospecting proactively reach out to potential clients through various channels such as cold calling, emails, networking events, and social media to generate leads and establish initial contact. <br /><br />
+                                                    Relationship building Build and maintain strong relationships with clients, key decision-makers, and stakeholders. Understand their needs, requirements, and pain points to provide suitable solutions and position the organization's offerings effectively. <br /><br />
+                                                    Client management act as a point of contact for clients, ensuring high levels of customer satisfaction. Address client concerns, resolve issues, and provide ongoing support to build long-term relationships and foster repeat business.
+                                                </p>
                                             </div>
-                                           
+
                                             <div className="mb-4">
                                                 <p> <strong>Required Skills And Abilities:</strong> upwork, freelancer, proposal writing</p>
                                             </div>
@@ -103,6 +100,47 @@ function Career() {
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-lg-12 mb-4'>
+                            <div className="accordion" id="accordionExample">
+                                <div className="accordion-item">
+                                    <h2 className="accordion-header" id="headingThree">
+                                        <button className="accordion-button shadow-none fw-bold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        Ads Manager 
+                                        </button>
+                                    </h2>
+                                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                        <div className="accordion-body pb-0">
+                                            <div className='mb-4'>
+                                                <h1 className='h5'>Roles And Responsibility:</h1>
+                                                <p>Plan, develop, and execute performance marketing campaigns across various digital channels such as Facebook Ads, Instagram Ads LinkedIn Ads, and other paid media platforms.</p>
+                                                <p>Monitor and optimize campaigns to ensure they meet predefined KPIs (Key Performance Indicators), such as ROAS (Return on Ad Spend) and CPA (Cost Per Acquisition).</p>
+                                                <p>Conduct research to identify relevant keywords, target audiences, and market trends to inform campaign strategy.</p>
+                                                <p>Collaborate with creative teams to develop effective ad creatives and copy that align with campaign objectives and target audience.</p>
+                                                <p>Manage advertising budgets, allocate resources effectively, and ensure campaigns are cost-efficient.</p>
+                                                <p>Plan and execute A/B tests to optimize ad creatives, landing pages, and other elements of the marketing funnel.</p>
+                                                <p>Proven experience in performance marketing, digital advertising, or a related role.</p>
+                                                <p>Proficiency in using digital marketing platforms and tools (e.g., LinkedIn marketing, Facebook Business Manager).</p>
+                                            </div>
+                                            
+                                            <div>
+                                                <p className='mb-1'><strong>Experience :</strong> Min 3 Years</p>
+                                                <p className='mb-1'><strong>No of Position :</strong>  1 </p>
+                                                <p className='mb-1'> <strong>Location :</strong>  Mohali</p>
+                                                <p className='mb-1'><strong>Interested Candidate call at :</strong><Link to="tel:+91 8427682603"> +91 8427682603</Link></p>
+
+                                            </div>
+
+                                        </div>
+                                        <div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
